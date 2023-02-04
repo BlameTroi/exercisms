0a3f580e78d822faa26bacc5f08966209002dfd0
troi <blametroi@gmail.com>
2023-02-04 12:06:17 -0500

    started fixing broken exercisms from their spec/test changes

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,6 +1,5 @@
 package wordcount
 
-//
 // func WordCount(phrase string) Frequency  // Implement this function.
 // type Frequency map[string]int            // Using this return type.
 
@@ -15,10 +14,14 @@ var (
 		'(', ')', '[', ']', '{', '}', '<', '>',
 		'"', '\\',
 	}
-	numbers    runeList = runeList{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	numbers    runeList = runeList{
+		'0', '1', '2', '3', '4',
+		'5', '6', '7', '8', '9'}
 	alphabetic runeList = runeList{
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 		'\'',
 	}
 )
@@ -49,19 +52,6 @@ func (rl runeList) tolower() runeList {
 	return lowered
 }
 
-func (rl runeList) unquote() runeList {
-	if len(rl) < 3 {
-		return rl
-	}
-	if rl[0] != '\'' && rl[0] != '"' {
-		return rl
-	}
-	if rl[len(rl)-1] == rl[0] {
-		return rl[1 : len(rl)-1]
-	}
-	return rl
-}
-
 func lowercase(r rune) rune {
 	if r >= 'A' && r <= 'Z' {
 		return r + 'a' - 'A'
@@ -73,16 +63,28 @@ func WordCount(phrase string) Frequency {
 	words := make(Frequency)
 	var word runeList = make(runeList, 0, 8)
 	for _, r := range phrase + " " {
+		if len(word) == 0 && (r == '\'' || r == '"') {
+			// don't put leading quotes on a word
+			continue
+		}
 		if breakers.contains(r) {
-			if len(word) != 0 {
-				if word.only(numbers) {
-					words[string(word)] += 1
-				} else if word.only(alphabetic) {
-
-					words[string(word.tolower().unquote())] += 1
-				}
-				word = make(runeList, 0, 8)
+			if len(word) == 0 {
+				// contiguous break characters
+				continue
 			}
+			for len(word) > 0 && (word[len(word)-1] == '\'' || word[len(word)-1] == '"') {
+				// trim any trailing quotes. this could produce an empty word.
+				word = word[:len(word)-1]
+			}
+			if len(word) == 0 {
+				continue
+			}
+			if word.only(numbers) {
+				words[string(word)] += 1
+			} else if word.only(alphabetic) {
+				words[string(word.tolower())] += 1
+			}
+			word = make(runeList, 0, 8)
 			continue
 		}
 		word = append(word, r)
