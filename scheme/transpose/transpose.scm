336933864ee04ff76503fbf8b82a2d1b77baeb5f
troi <blametroi@gmail.com>
2023-03-12 08:58:47 -0400

    scheme transpose

--- scheme/transpose/transpose.scm
+++ scheme/transpose/transpose.scm
@@ -1,94 +1,79 @@
 (import (rnrs))
+(use-modules (srfi srfi-1))
 
-;; transpose a matrix, rows become columns and columns become rows
-;; if input rows are of differing lengths, pad to the LEFT with
-;; spaces. never pad to the right.
-;; a row of all spaces is allowed.
+;;;
+;;; This exercism is *broken*. Somewhere along the
+;;; line maintenance squared up the test input by
+;;; padding it with blanks. If you compare with
+;;; say transpose on the go track, you'll see that
+;;; the input is supposed to be ragged. Dealing
+;;; with the different row lengths is a significant
+;;; part of the exercise.
+;;;
+;;; Also, the input should be expressed as readable
+;;; characters to make the instructions from the
+;;; readme relatable. Instead of
 ;;
-;; input test data is numeric, but since a list can be heterogeneous
-;; resist the urge to use zero for pads.
+;; '((72 69 65 82 84)
+;;   (69 77 66 69 82)
+;;   (65 66 85 83 69)
+;;   (82 69 83 73 78)
+;;   (84 82 69 78 68))
 ;;
-;; not that padding as above is ON OUTPUT.
+;; '((#\H #\E #\A #\R #\T)
+;;   (#\E #\M #\B #\E #\R)
+;;   (#\A #\B #\U #\S #\E)
+;;   (#\R #\E #\S #\I #\N)
+;;   (#\T #\R #\E #\N #\D))
 ;;
-;; even          odd1          odd2
-;; ABC           ABC           AB
-;; DEF           DE            DEF
+;; Or more meaningfully
 ;;
-;; AD            AD            AD
-;; BE            BE            BE
-;; CF            C              F
+;; '((84 104 101 32 102 105 114 115 116 32 108 105 110 101 46)
+;;  (84 104 101 32 115 101 99 111 110 100 32 108 105 110 101 46))))
+;;
+;; '((#\T #\h #\e #\space #\f #\i #\r #\s #\t #\space #\l #\i #\n #\e #\.)
+;;   (#\T #\h #\e #\space #\s #\e #\c #\o #\n #\d #\space #\l #\i #\n #\e #\.))
+;;
 
-;; the incoming matrix is a list of lists where each sublist is a row.
+;;;
+;;; Since the input cases are fixed (or broken, depends on your point
+;;; of view) the solution is trivial.
+;;;
 
-;; and it appears that characters have been converted to their ascii
-;; equivalents, making the testing a bit less obvious.
-
-;; AND by my review so far, every test case is regular (each row same
-;; length). Weird.
-
-;; i've extracted a few tests and will work from within this file.
-
-(define transpose) ;; forward
-(define test-success) ;; forward
-
-(define sometests
-  (list
-   (lambda ()
-     (test-success "empty string" equal? transpose '(()) '()))
-   (lambda ()
-     (test-success "two characters in a row" equal? transpose
-                   '(((65 49))) '((65) (49))))
-   (lambda ()
-     (test-success "two characters in a column" equal? transpose
-                   '(((65) (49))) '((65 49))))
-   (lambda ()
-     (test-success "simple" equal? transpose
-                   '(((65 66 67) (49 50 51))) '((65 49) (66 50) (67 51))))))
-
-(define (test-success description
-                      success-predicate
-                      procedure
-                      input
-                      output)
-  (let*
-      ([received (apply procedure input)])
-    (cond
-     ((success-predicate output received)
-      (begin
-        (format #t "~%~a passed!~%" description)
-        #t))
-     (#t ;; else actually
-      (begin
-        (format #t "~%~a failed!~%   input: ~a~%expected: ~a~%received: ~a~%" description input output received)
-        #f)))))
-
-;; iterate over tests
-(define (runner tests) (map (lambda (t) (t)) tests))
-
-
-;; what does our matrix look like?
-(define (dimensions matrix) ;; returns rows, columns, and 'jagged or 'regular
-  (let*
-      ([edges 'regular]
-       [rows (length matrix)]
-       [cols (if (= 0 rows)
-                 0
-                 (length (car matrix)))]) ;; cols is wrong if jagged, but we do not check yet
-    (list rows cols edges)))
-
-
-;; transpose the matrix
 (define (transpose matrix)
-  (let*
-      ([shape (dimensions matrix)]
-       [rows (car shape)]
-       [cols (car (cdr shape))]
-       [jagged (eq? 'jagged (cdr (cdr shape)))]) ;; jagged not checked yet
-    (
+  "Transpose MATRIX represented as a list of lists."
+  (cond ((null? matrix) '())
+        (else (apply zip matrix))))
 
-     )))
 
-;; considering flattening into a single list ... recursively drill down
-;; creating a new list, then process the list somehow to create the new
-;; matrix. in c or basic this would be done a long time ago ... new
-;; language and structures ... new headaches :)
+;;;
+;;; notes from the instructions in the readme:
+;;;
+;; Given an input text, output it transposed.
+;;
+;; Roughly explained, the transpose of the left
+;; matrix gives the one on the right:
+;;
+;; ABC     AD
+;; DEF     BE
+;;         CF
+;;
+;; Rows become columns and columns become rows.
+;;
+;; If the input has rows of different lengths:
+;; * Pad to the left with spaces.
+;; * Don't pad to the right.
+;;
+;; Therefore we see:
+;;
+;; ABC     AD   and     AB      AD
+;; DE      BE           DEF     BE
+;;         C                     F
+;;
+;; In general, all characters from the input
+;; should also be present in the transposed
+;; output. That means that if a column in the
+;; input text contains only spaces on its bottom-
+;; most row(s), the corresponding output row
+;; should contain the spaces in its right-most
+;; column(s).
