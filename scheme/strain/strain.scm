bec3a88a7e01d209a40d50a5b56123e6251ecaea
troi <blametroi@gmail.com>
2023-04-02 22:43:56 -0400

    scheme strain done, init triangle

--- scheme/strain/strain.scm
+++ scheme/strain/strain.scm
@@ -1,7 +1,17 @@
 (import (rnrs))
 
 (define (keep pred seq)
-  'implement-me!)
+  (letrec* ((f (lambda (xs accum)
+                 (cond
+                  ((null? xs) (reverse accum))
+                  ((apply pred (list (car xs))) (f (cdr xs) (cons (car xs) accum)))
+                  (else (f (cdr xs) accum))))))
+    (f seq '())))
 
 (define (discard pred seq)
-  'implement-me!)
+  (letrec* ((f (lambda (xs accum)
+                 (cond
+                  ((null? xs) (reverse accum))
+                  ((not (apply pred (list (car xs)))) (f (cdr xs) (cons (car xs) accum)))
+                  (else (f (cdr xs) accum))))))
+    (f seq '())))
