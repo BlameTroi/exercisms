650e7172fcd55bd91c731c2c17448cbd326c1187
troi <blametroi@gmail.com>
2023-04-03 22:43:48 -0400

    scheme pangram done, init collatz

--- scheme/pangram/pangram.scm
+++ scheme/pangram/pangram.scm
@@ -1,5 +1,15 @@
 (import (rnrs))
 
 (define (pangram? phrase)
-  'implement-me!)
-
+  "Is string PHRASE a pangram? This is case insensitive."
+  (letrec*
+      ((full (string->list (string-downcase phrase)))
+       (letters-only (filter (lambda (c) (and (char>=? c #\a) (char<=? c #\z))) full))
+       (only-one (lambda (cs accum)
+                   (cond
+                    ((null? cs) accum)
+                    (else
+                     (only-one (cdr cs) (if (not (member (car cs) accum))
+                                            (cons (car  cs) accum)
+                                            accum)))))))
+    (= 26 (length (only-one letters-only '())))))
